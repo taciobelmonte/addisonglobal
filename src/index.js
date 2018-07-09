a3964b5bae6281e697037f1fabe8782bb89f304d
TACIO <tacio@effectdigital.com>
2018-07-09 00:30:05 -0300

    [fix] - removes logger

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
-import {Provider} from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import reducer from './reducers'
 import thunk from 'redux-thunk';
+import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import './assets/css/main.css';
 import './assets/css/responsive.css';
 import App from './components/App'
-
-const logger = store => next => action => {
- console.group(action.type);
- console.info('dispatching', action);
- let result = next(action);
- console.log('next state', store.getState());
- console.groupEnd(action.type);
- return result
- };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -25,7 +16,6 @@ const store = createStore(
     reducer,
     composeEnhancers(
         applyMiddleware(thunk),
-        applyMiddleware(logger)
     )
 );
 
